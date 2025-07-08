a04f6979fb7a2eb751385e2d206fab1ae343f2bf
Eliabe Mota <eliaberibeiro06@gmail.com>
2025-07-08 17:12:46 -0300

    sf php novo_recibo

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -13,19 +13,29 @@
 	<form action="" method="get" style="background: red;">
 		<label>ID_RECIBO</label>
 		<input type="text" value="0" name="id_recibo" id="id_recibo">
-		
-		<br>
 
 		<label>Nome do Contribuinte</label>
 		<input type="text" name="nome" id="nome">
 
+		<br>
 		<label>Tipo de receita</label>
 		<select>
 			<option>Dízimo</option>
 			<option>Coleta</option>
 			<option>Missão</option>
-			<
+			<option>Oferta</option>
+			<option>Voto</option>
 		</select>
+
+		<label>Valor</label>
+		<input type="text" name="valor" id="valor">
+
+		<br>
+
+		<label>Data</label>
+		<input type="data" name="dia_recibo" id="dia_recibo">
+
+		<button name="salvar">Cadastrar</button>
 	</form>
 </main>
 <footer>rodape</footer>
