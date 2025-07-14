96940b04e83105d7e1e6acb71babe7a9d59753e4
Eliabe Mota <eliaberibeiro06@gmail.com>
2025-07-14 09:42:01 -0300

    alterações do cliente14-07

--- resources/views/pdf/recibo.blade.php
+++ resources/views/pdf/recibo.blade.php
@@ -15,7 +15,7 @@
 	</style>
 </head>
 <body>
-	<br><br><br>
+	<hr><br><br><br>
 	
 	<!-- Primeira Via -->
 	<p>
@@ -30,19 +30,21 @@
 		E-MAIL: adpontealtadonorte@gmail.com
 		<br>FACEBOOK: AD Ponte
 	</p>
-		<h1>Impressão de Recibo digital nº {{$receita->id}}</h1>
+		<h1>Impressão de Recibo digital nº 00{{$receita->id}}</h1>
 		<p>Nome do Contribuinte: {{$receita->NOME}}</p>
 		<p>Valor: {{$receita->VALOR}},00</p>
 		<p>Tipo de contribuição: {{$receita->TIPO}}</p>
 		<p>Data de emissão: {{$receita->DATA}}</p>
 
-	<p>1ª Via - Contabilidade</p>
-	<p style="font-size: 0.8rem; text-align: right;">Desenvolvido por Eliabe Ribeiro Mota. 2025-2025</p>
-	<br><br><hr></hr><br><br>
+		<p>1ª Via - Contribuinte</p>
+		<p style="font-size: 0.8rem; text-align: right;">Desenvolvido por Eliabe Ribeiro Mota. 2025-2025</p>
+	<br><br><hr><br><br>
 	
 	<!-- Segunda via -->
 	<p>
 		IGREJA EVANGÉLICA ASSEMBLÉIA DE DEUS DE PONTE ALTA DO NORTE
+		<br>
+		CNPJ: 11.101.212/0001-81
 		<br>
 		RUA TRINTA DE MARÇO Nº 61 – SÃO SEBASTIÃO 
 		<br>
@@ -57,7 +59,7 @@
 		<p>Tipo de contribuição: {{$receita->TIPO}}</p>
 		<p>Data de emissão: {{$receita->DATA}}</p>
 
-	<p>2ª Via - Contribuinte</p>
+	<p>2ª Via - Contabilidade</p>
 	<p style="font-size: 0.8rem; text-align: right;">Desenvolvido por Eliabe Ribeiro Mota. 2025-2025</p>
 	
 
